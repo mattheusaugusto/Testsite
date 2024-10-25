c29c743a91ada8db0275cccc40eb7c1f264b055f
MATHEUS BARBOSA <MATHEUS_BARBOSA@jabil.com>
2024-10-25 14:00:48 -0300

    Add Project init

---
+++ javascript-basico-starwars/JS/app.js
@@ -0,0 +1,91 @@
+// console.log('olá, Javascript')
+
+
+var userName = 'Matheus Barbosa'
+
+document.getElementById('user-name').innerHTML = userName
+
+//Variaveis//
+
+// var nome = 'Mestre Yoda'
+// var idade = 100
+// var jedi = true 
+
+//Operadores Matematicos//
+
+// var n1 = 10
+// var n2 = 1.5
+// // + para somar 
+// // - para subtrair
+// // * para multiplicar
+// // / para dividir
+
+// var total = n1 / n2 
+// console.log(total)
+
+//Operadores de comparação//
+// var v1= 5
+// var v2= '5'
+
+// var resultado = v1 != v2 
+// console.log(resultado)
+
+// == igual 
+// === igual estrito
+// != diferente
+// !== diferente estrito
+
+//funções//
+// function soma (n1,n2) {
+// console.log(n1+n2)
+// }
+
+// soma(4,5)
+
+// function Welcome(){
+//     alert('Welcome :)');
+// }
+// Welcome();
+
+// function soma (n1,n2){
+// return n1 + n2;
+// }
+
+// var resultado = soma(5,4);
+// console.log(resultado)
+
+//Controle de fluxos BDD//
+
+//Array//
+
+// var personagem = ['choosen undead','Maculado','Ashe one','Caçador', 'lobo']
+// personagem.push('Bearer of the curse')
+// personagem.push('Bayonetta')
+// personagem.pop()
+// personagem = personagem.filter(function(p){
+//     return p !== 'lobo'
+// })
+// console.log(personagem)
+
+//Controle de repetições//
+// var personagem = ['Bayonetta','Dante','Vergil','Kratos']
+// personagem.forEach(function(p){
+//     console.log(p);
+// })
+
+//super variaveis ou objetos//
+// var JogosBons = {
+//     Jogo1 : 'Bayonetta',
+//     Jogo2 : 'Devil May Cry',
+//     Jogo3 : 'Dark Souls',
+//     Jogo4 : 'Bloodborne',
+// }
+// console.log (JogosBons)
+
+//Constantes//
+
+// const name = 'Bayoneta'
+// console.log (name)
+
+// name = 'Dante'
+// console.log (name)
